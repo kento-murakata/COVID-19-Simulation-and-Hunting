6949c9f4e82fc5361ec4ed1e2010412e063a58b9
Kento Murakata <kento_murakata@woven-planet.global>
2022-09-14 15:46:41 +0900

    Initial Commit

---
+++ Assets/Scripts/PersonBehaviour.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PersonBehaviour : MonoBehaviour
+{
+    public GameObject person;
+    public float maxPositionX = 25.0f;
+    public float minPositionX = -25.0f;
+    public float maxPositionZ = 25.0f;
+    public float minPositionZ = -25.0f;
+    public float randomTime = 10.0f;
+
+    private Vector3 targetPosition;
+    private float time;
+
+
+    private void Awake()
+    {
+        targetPosition = new Vector3 (
+            Random.Range(minPositionX,maxPositionX),
+            1,
+            Random.Range(minPositionZ,maxPositionZ));
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+
+        if(time <= randomTime)
+        {
+            transform.position = Vector3.MoveTowards(
+                transform.position, 
+                targetPosition, 
+                0.01F);
+        }
+        else
+        {
+            time = 0;
+            targetPosition = new Vector3(
+            Random.Range(minPositionX, maxPositionX),
+            1,
+            Random.Range(minPositionZ, maxPositionZ));
+        }
+
+
+    }
+   
+}
