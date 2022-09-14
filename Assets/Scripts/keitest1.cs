17674eaac8783d72675e5e2d38645f2f27f8df7f
kei.kawasaki <kei.kawasaki@woven-planet.global>
2022-09-14 16:53:31 +0900

    Test2

--- Assets/Scripts/keitest1.cs
+++ Assets/Scripts/keitest1.cs
@@ -7,7 +7,7 @@ public class keitest1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Test");
     }
 
     // Update is called once per frame
