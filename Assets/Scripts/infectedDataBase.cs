9568522323b08f399cba43daa09a9a5d8fad316e
hidetoonitsuka <hideto.onitsuka@woven-planet.global>
2022-09-15 16:33:03 +0900

    Add some code to infectedDatabase.cs

--- Assets/Scripts/infectedDataBase.cs
+++ Assets/Scripts/infectedDataBase.cs
@@ -1,18 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
-public class infectedDataBase : MonoBehaviour
+
+namespace InfectionSimulator
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //    /*
+    //    1日当たりの陽性数 List
+    //    1日当たりの死者数 List
+    //    トータル死者数。１日目のTOTAL、２日目のTOTALみたいに日々のトータルを出す
+    //    トータル感染者。上と一緒
+    //    病院使用率
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //     */
+    //    public class infectedDataBase : MonoBehaviour
+    //    {
+    //        public enum infectedStatus
+    //        {
+    //            NEGATIVE,
+    //            EXPOSE,
+    //            ASYMPOTOMATIC,
+    //            SYMPTONS,
+    //            INFECTED;
+
+
+    //        }
+    //        public void Day()
+    //        {
+    //            int dayCount; // ～日目の "～"の部分の変数
+    //            dayCount = 1;
+    //            string day = $"Day{dayCount}"; //Day1 を day に代入
+    //        }
+
+    //        public void CountDailyInfector()
+    //        {
+    //            int[] dailyInfector = new int[];
+    //        }
+
+    //        public void CountDailyDeath()
+    //        {
+    //            int[] dailydeath = new int[];
+    //        }
+
+    //        public void CountTotalInfector()
+    //        {
+    //            dailyInfector.CountDailyInfector();
+    //            num.Sum(); // 15
+    //            int totalInfector =
+
+    //        }
+
+    //        public void CountTodalDeath()
+    //    {
+    //        int totalDeath;
+    //    }
+
+
 }
+
+
+
+
+
+
+
+
+
