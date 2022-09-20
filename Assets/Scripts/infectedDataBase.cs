aebece08363350a6a261c6240545240a3082bc0d
hidetoonitsuka <hideto.onitsuka@woven-planet.global>
2022-09-20 13:04:30 +0900

    0920AM

--- Assets/Scripts/infectedDataBase.cs
+++ Assets/Scripts/infectedDataBase.cs
@@ -6,74 +6,26 @@ using System.Linq;
 
 namespace InfectionSimulator
 {
-    //    /*
-    //    1日当たりの陽性数 List
-    //    1日当たりの死者数 List
-    //    トータル死者数。１日目のTOTAL、２日目のTOTALみたいに日々のトータルを出す
-    //    トータル感染者。上と一緒
-    //    病院使用率
-
-    //floatのリスト=> Graphへ
-
-    //     */
-    //    public class infectedDataBase : MonoBehaviour
-    //    {
-    //        public enum infectedStatus
-    //        {
-    //            NEGATIVE,
-    //            EXPOSE,
-    //            ASYMPOTOMATIC,
-    //            SYMPTONS,
-    //            INFECTED;
-    //        }
-    public class EnemyManager : MonoBehaviour
+    public class DataBase : MonoBehaviour
     {
-        GameObject[] enemyObjects;
-        int enemyNum;
-        void Start()
+        public void DailyInfectedNumber()
         {
-            enemyObjects = GameObject.FindGameObjectsWithTag("Enemy");
-            enemyNum = enemyObjects.Length
+            int infector = 0;//GameMangerから感染者数持ってきて置き換える。
+            int timeCounter = 100; //日ごとか月ごとか何かしらの区切り要素？
+
+            string[] day = new string[] { };
+            int dayCount=0;            
+            for(int i=0; i<timeCounter; i++)
+            {
+                dayCount = dayCount + 1;
+                day[i]= "Day{dayCount}";
+            }
+            
+            int[] infectedNumber = new int[] { };
+            for (int i = 0; i<timeCounter; i++)
+            {
+                infectedNumber[i] = 1;
+            }
         }
     }
-    public void Day()
-    {
-        int dayCount; // ～日目の "～"の部分の変数
-        dayCount = 1;
-        string day = $"Day{dayCount}"; //Day1 を day に代入
-    }
-
-    //        public void CountDailyInfector()
-    //        {
-    //            int[] dailyInfector = new int[];
-    //        }
-
-    //        public void CountDailyDeath()
-    //        {
-    //            int[] dailydeath = new int[];
-    //        }
-
-    //        public void CountTotalInfector()
-    //        {
-    //            dailyInfector.CountDailyInfector();
-    //            num.Sum(); // 15
-    //            int totalInfector =
-
-    //        }
-
-    //        public void CountTodalDeath()
-    //    {
-    //        int totalDeath;
-    //    }
-
-
 }
-
-
-
-
-
-
-
-
-
